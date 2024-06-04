83b22b5c43329ff000356458f2a43a83493b5d9a
MedAbdmouleh <Mohamed.Abdmouleh@studentambassadors.com>
2024-06-04 13:34:56 +0200

    add changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -31,12 +31,12 @@ const description =
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Power BI",
+  "Power Automate",
+  "Azure",
+  "Data Factory ",
+  "Fabric",
+  "Office 365",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems through innovative and creative approaches. By leveraging my experience as a data analyst, I continually look for new and better ways to make technology accessible to all. My focus is on analyzing data to uncover insights that drive engagement, enhance user experience, and ultimately lead to greater inclusivity in tech.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/computer.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "desktop laptop";
 
 /**
  * Project list
@@ -31,16 +31,16 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "JavaScript Portfolio",
     description:
       "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    url: "https://github.com/MedAbdmouleh/Student",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Training Power BI",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Training Power BI in Tunisia",
+    url: "https://stdntpartners-my.sharepoint.com/personal/mohamed_abdmouleh_studentambassadors_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fmohamed%5Fabdmouleh%5Fstudentambassadors%5Fcom%2FDocuments%2FFormation%20Power%20BI%20Orange",
   },
   {
     title: "My Resume Site",
@@ -49,10 +49,10 @@ const projectList = [
     url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Training Power BI with pratice exemple",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Formation en #business #intelligence #Microsoft #BI en mode #projet",
+    url: "https://www.youtube.com/watch?v=eUTNminjuSY",
   },
 ];
 
