cc654e90798ae469f380158fd02a7e6f9d5c2adb
MedAbdmouleh <Mohamed.Abdmouleh@studentambassadors.com>
2024-07-22 17:42:55 +0200

    Changes26

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -24,7 +24,7 @@ const Home = ({ name, title }) => {
         <h1>{name}</h1>
         <h2>{title}</h2>
       </div>
-      <div style={{ position: "absolute", bottom: "3rem", left: "50%" ,textAlign: "right"}}>
+      <div style={{ position: "absolute", top: "5rem", right: "2rem", width: "20rem",textAlign: "right"}}>
         <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
       </div>
       <div style={{ position: "absolute", top: "5rem", right: "2rem", width: "20rem", textAlign: "right" }}>
