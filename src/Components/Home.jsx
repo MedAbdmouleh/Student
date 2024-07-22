139e053e5464e3b0df083d5db253ac2a5429b1b3
MedAbdmouleh <Mohamed.Abdmouleh@studentambassadors.com>
2024-07-22 21:36:52 +0200

    Changes44

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,9 +9,11 @@ const Home = ({ name, title }) => {
   return (
     <section id="home" style={{ position: "relative", display: "flex", alignItems: "center", justifyContent: "space-between", minHeight: "100vh", padding: "2rem" }}>
       <div style={{ position: "relative", zIndex: 1, maxWidth: "50%", marginRight: "2rem" }}>
-        <h1>{name}</h1>
-        <h2>Consultant MSBI & Azure Data Factory & Power BI</h2>
-        <h3>Consultant and trainer specializing in Power BI and Power Platform, I have extensive experience in designing, developing, and implementing business intelligence solutions.</h3>
+        <h4>Hi I am</h4>
+        <h3 style={{ color: "#FFA500" }}>{name}</h3>
+        <h2 style={{ color: "#FFA500" }}>{title}</h2>
+        <br />
+        <h6>Consultant and trainer specializing in Power BI and Power Platform, I have extensive experience in designing, developing, and implementing business intelligence solutions.</h6>
         <button style={{ marginTop: "1rem", padding: "0.5rem 1rem", fontSize: "1rem", cursor: "pointer", backgroundColor: "#FFA500", color: "#fff", border: "none", borderRadius: "4px" }}>
           Service
         </button>
@@ -25,8 +27,8 @@ const Home = ({ name, title }) => {
 };
 
 Home.defaultProps = {
-  name: "",
-  title: "",
+  name: "Abdmouleh Mohamed",
+  title: "Consultant MSBI & Azure Data Factory & Power BI",
 };
 
 Home.propTypes = {
