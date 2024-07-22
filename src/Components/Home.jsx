8e9c46d8c6cfd611c923b71c131e1d4d29251268
MedAbdmouleh <Mohamed.Abdmouleh@studentambassadors.com>
2024-07-22 20:09:08 +0200

    Change37

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,34 +1,23 @@
 import React from "react";
 import arrowSvg from "../images/down-arrow.svg";
 import PropTypes from "prop-types";
+import image from "../images/medorange.png";
 
-/**
- * Home background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
- * represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
-import image from "../images/medmed.png";
-
-const imageAltText = "";
+const imageAltText = "Consultant MSBI & Azure Data Factory & Power BI";
 
 const Home = ({ name, title }) => {
   return (
-    <section id="home" className="min-height" style={{ position: "relative", display: "flex", alignItems: "center" }}>
-      <img className="background" src={image} alt="" style={{ width: "100%", height: "100%", objectFit: "cover", position: "absolute", zIndex: -1 }} />
-      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
+    <section id="home" style={{ position: "relative", display: "flex", alignItems: "center", justifyContent: "space-between", padding: "2rem" }}>
+      <div style={{ position: "relative", zIndex: 1 }}>
         <h1>{name}</h1>
         <h2>{title}</h2>
+        <button style={{ marginTop: "1rem", padding: "0.5rem 1rem", fontSize: "1rem", cursor: "pointer", backgroundColor: "#007BFF", color: "#fff", border: "none", borderRadius: "4px" }}>
+          Learn More
+        </button>
       </div>
+      <img src={image} alt={imageAltText} style={{ width: "50%", height: "auto", objectFit: "cover", position: "absolute", right: 0, top: 0, zIndex: -1 }} />
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
         <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} />
-      </div>
-      <div style={{ position: "absolute", top: "5rem", right: "2rem", width: "20rem", textAlign: "right", fontSize: "1.5rem" }}>
-        <p><b>Abdmouleh Mohamed - Consultant Power BI</b></p>
       </div>
     </section>
   );
