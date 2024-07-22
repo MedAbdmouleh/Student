6d15305fae7d8e407276f8ee8e217089080eb3bb
MedAbdmouleh <Mohamed.Abdmouleh@studentambassadors.com>
2024-07-22 16:02:48 +0200

    Change18

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,58 +1,39 @@
+/**
+ * Home component
+ *
+ * The section at the top of the page to display image of your
+ * choice, name and title that describes your career focus.
+ */
+
 import React from "react";
 import arrowSvg from "../images/down-arrow.svg";
+import PropTypes from "prop-types";
+
+/**
+ * Home background image
+ *
+ * Below is a sample image. Upload the image of your choice into the "images"
+ * directory and import here for use. Then, set imageAltText to string that 
+ * represents what you see in that image.
+ *
+ *
+ * Need an image? Check out https://unsplash.com to download a photo you
+ * freely use on your site.
+ */
 import image from "../images/med.jpg";
 
-const imageAltText = "Abdmouleh Mohamed - Consultant Power BI";
-
-const homeStyles = {
-  hero: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: '2em 0',
-    backgroundColor: '#fff',
-    position: 'relative',
-  },
-  heroContent: {
-    flex: 1,
-  },
-  heroTitle: {
-    fontSize: '2.5em',
-  },
-  heroSpan: {
-    color: '#ff6600',
-  },
-  heroSubtitle: {
-    color: '#666',
-  },
-  heroText: {
-    margin: '1em 0',
-  },
-  heroButton: {
-    backgroundColor: '#ff6600',
-    color: '#fff',
-    padding: '0.5em 1em',
-    textDecoration: 'none',
-    borderRadius: '5px',
-  },
-  heroImage: {
-    flex: 1,
-  },
-  heroImageImg: {
-    width: '100%',
-    borderRadius: '10px',
-  },
-};
+const imageAltText = " Abdmouleh Mohamed - Consultant Power BI";
 
 const Home = ({ name, title }) => {
   return (
-    <section id="home" style={homeStyles.hero}>
-      <img className="background" src={image} alt="" style={homeStyles.heroImageImg} />
-      <div style={{ position: 'absolute', top: '5rem', left: '2rem', width: '17rem' }}>
-        <h1 style={homeStyles.heroTitle}>{name}</h1>
-        <h2 style={homeStyles.heroSubtitle}>{title}</h2>
+    <section id="home" className="min-height">
+      <img className="background" src={image} alt="" />
+      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
+        <h1>{name}</h1>
+        <h2>{title}</h2>
       </div>
-      <div style={{ position: 'absolute', bottom: '3rem', left: '50%' }}>
-        <img src={arrowSvg} style={{ height: '3rem', width: '3rem' }} alt={imageAltText} />
+      <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
+        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
       </div>
     </section>
   );
@@ -61,6 +42,11 @@ const Home = ({ name, title }) => {
 Home.defaultProps = {
   name: "",
   title: "",
+};
+
+Home.propTypes = {
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 };
 
 export default Home;
