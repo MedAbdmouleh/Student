04a57b606d18b40a84a336a82782367ccc7c7fe0
MedAbdmouleh <Mohamed.Abdmouleh@studentambassadors.com>
2024-07-27 14:57:19 +0200

    c22

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,56 +1,21 @@
 import React from "react";
 import arrowSvg from "../images/down-arrow.svg";
+import PropTypes from "prop-types";
 import image from "../images/DSC_0617.jpg";
 
+
+const imageAltText = "Consultant MSBI & Azure Data Factory & Power BI";
+
 const Home = ({ name, title }) => {
-  const { width } = useWindowSize(); // Assume useWindowSize hook is defined as shown earlier
-
-  const homeStyles = {
-    section: {
-      position: "relative",
-      display: "flex",
-      flexDirection: width < 768 ? 'column' : 'row',
-      alignItems: "center",
-      justifyContent: "space-between",
-      minHeight: "100vh",
-      padding: width < 768 ? '2rem' : '4rem',
-    },
-    textContainer: {
-      position: "relative",
-      zIndex: 1,
-      maxWidth: width < 768 ? '90%' : '50%',
-      marginRight: width < 768 ? '0' : '2rem',
-      textAlign: width < 768 ? 'center' : 'left',
-    },
-    heading: {
-      color: "#FFA500",
-      fontSize: width < 768 ? '1.5em' : '2em',
-    },
-    image: {
-      width: width < 768 ? '100%' : '50%',
-      height: "auto",
-      objectFit: "cover",
-      position: "absolute",
-      right: 0,
-      top: 0,
-      zIndex: -1,
-    },
-    arrow: {
-      position: "absolute",
-      bottom: "3rem",
-      left: "50%",
-      transform: "translateX(-50%)",
-      height: "3rem",
-      width: "3rem",
-    },
-  };
-
   return (
-    <section style={homeStyles.section} id="home">
-      <div style={homeStyles.textContainer}>
+    <section id="home" style={{ position: "relative", display: "flex", alignItems: "center", justifyContent: "space-between", minHeight: "100vh", padding: "2rem" }}>
+      <link rel="stylesheet" href="/src/images/styles.css" />
+      <div style={{ position: "relative", zIndex: 1, maxWidth: "50%", marginRight: "2rem" }}>
         <h4>Hi I am</h4>
-        <h2 style={homeStyles.heading}>{name}</h2>
-        <b><h2 style={homeStyles.heading}>{title}</h2></b>
+        <h2 style={{ color: "#FFA500" }}>Abdmouleh Mohamed</h2>
+        <br/>
+        <b><h2 style={{ color: "#FFA500" }}>Consultant MSBI & Azure Data Factory & Power BI</h2></b>
+        <br />
         <h4>Consultant and trainer specializing in Power BI and Power Platform.</h4>
         <a href="#services">
           <button style={{ marginTop: "1rem", padding: "0.5rem 1rem", fontSize: "1rem", cursor: "pointer", backgroundColor: "#FFA500", color: "#fff", border: "none", borderRadius: "4px" }}>
@@ -58,9 +23,9 @@ const Home = ({ name, title }) => {
           </button>
         </a>
       </div>
-      <img src={image} alt="Consultant MSBI & Azure Data Factory & Power BI" style={homeStyles.image} />
-      <div style={homeStyles.arrow}>
-        <img src={arrowSvg} alt="Scroll down arrow" />
+      <img src={image} alt={imageAltText} style={{ width: "50%", height: "auto", objectFit: "cover", position: "absolute", right: 0, top: 0, zIndex: -1 }} />
+      <div style={{ position: "absolute", bottom: "3rem", left: "50%", transform: "translateX(-50%)" }}>
+        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt="Scroll down arrow" />
       </div>
     </section>
   );
@@ -69,6 +34,11 @@ const Home = ({ name, title }) => {
 Home.defaultProps = {
   name: "Abdmouleh Mohamed",
   title: "Consultant MSBI & Azure Data Factory & Power BI",
+};
+
+Home.propTypes = {
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 };
 
 export default Home;
